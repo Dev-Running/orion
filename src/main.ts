f1de0ae63d67216b3f2137e4d8969b423c01a90f
laurentino14 <laurentino14@outlook.com>
2022-11-24 23:04:12 -0300

    Module Alias

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { AppModule } from '@/app.module'
 import { NestFactory } from '@nestjs/core'
+import "module-alias/register"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: { origin: '*' } })
   await app.listen(3232)
